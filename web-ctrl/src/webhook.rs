6601c7d4f43b50895d0a9772ce463a30d7d3f0f3
Penn Bauman <pennbauman@protonmail.com>
2021-01-09 02:49:07 -0500

    added webhook verification

--- web-ctrl/src/webhook.rs
+++ web-ctrl/src/webhook.rs
@@ -2,18 +2,36 @@
 //   url: https://github.com/pennbauman/server
 //   author:
 //     penn bauman (pennbauman@protonmail.com)
-use tide::{Request, Response};
+use tide::{Request, Response, Body};
 use serde_json::Value;
 use std::process::Command;
 use crate::config;
+use sha2::{Sha256, Digest};
 
 pub async fn routes(app: &mut tide::Server<()>) {
     app.at("/pennbauman-com/webhook").post(|mut req: Request<()>| async move {
-        let body: Value = req.body_json().await?;
-        if body["repository"]["name"] != "pennbauman.com" {
+        let body_data = req.body_bytes().await?;
+
+        let check_hash = match req.header("X-Hub-Signature-256") {
+            Some(r) => &r[0],
+            None => return Ok(Response::new(406)),
+        };
+        println!("github: {}", check_hash);
+        let mut body_hash = Sha256::new();
+        body_hash.update("KG4tXyewSXAP8H8fC6tTMnQ8uaNxyjo8");
+        body_hash.update(&body_data);
+        print!("calced: sha256=");
+        for i in body_hash.finalize() {
+            print!("{:02x}", i);
+        }
+        println!("");
+
+        let body_copy = Body::from_bytes(body_data);
+        let body_vals: Value = body_copy.into_json().await?;
+        if body_vals["repository"]["name"] != "pennbauman.com" {
             return Ok(Response::new(406));
         }
-        let full_ref = match body["ref"].as_str() {
+        let full_ref = match body_vals["ref"].as_str() {
             Some(r) => r,
             None => return Ok(Response::new(406)),
         };
@@ -25,15 +43,18 @@ pub async fn routes(app: &mut tide::Server<()>) {
         let mut script = config::pennbauman_dir().await?;
         script.push_str("/control.sh");
 
-        let mut update = Command::new("bash");
-        update.arg(script).arg(branch).arg("&");
-        update.spawn();
-
-        //let result = update.output().expect("failed to execute process");
-        //if ! result.status.success() {
-            //return Ok(Response::new(417));
-        //}
+        update(&script, branch).await;
 
         Ok(Response::new(200))
     });
+}
+
+#[allow(unused_must_use)]
+async fn update(script: &str, branch: &str) {
+    let mut cmd = Command::new("bash");
+    cmd.arg(script).arg(branch).arg("&");
+    cmd.spawn();
+    //if ! result.status.success() {
+        //return Ok(Response::new(417));
+    //}
 }
--- web-ctrl/test/test.sh
+++ web-ctrl/test/test.sh
@@ -3,17 +3,18 @@
 
 curl http://localhost:8070/pennbauman-com/webhook \
 	--header "Content-Type: application/json" \
+	--header "X-Hub-Signature-256: sha256=31a1c2ba184720c70f6c6f0f1fc896d9ca22fe71e614677f71aaa2371dd0edd3" \
 	--request POST \
 	--data '{
-  "ref": "refs/heads/master",
-  "before": "0000000000000000000000000000000000000000",
-  "after": "bb711f5318db6593eed8ca00f0ae7d81f9d22dd7",
+  "ref": "refs/heads/tide",
+  "before": "7acc913918bb4a7e6ab5637bb84bac048d1d4d92",
+  "after": "99c422d642687a2dc2808263fba014444c3a9d7d",
   "repository": {
-    "id": 327445030,
-    "node_id": "MDEwOlJlcG9zaXRvcnkzMjc0NDUwMzA=",
-    "name": "test",
-    "full_name": "pennbauman/test",
-    "private": true,
+    "id": 158505281,
+    "node_id": "MDEwOlJlcG9zaXRvcnkxNTg1MDUyODE=",
+    "name": "pennbauman.com",
+    "full_name": "pennbauman/pennbauman.com",
+    "private": false,
     "owner": {
       "name": "pennbauman",
       "email": "pennbauman@protonmail.com",
@@ -36,58 +37,58 @@ curl http://localhost:8070/pennbauman-com/webhook \
       "type": "User",
       "site_admin": false
     },
-    "html_url": "https://github.com/pennbauman/test",
-    "description": null,
+    "html_url": "https://github.com/pennbauman/pennbauman.com",
+    "description": "Source files for personal website.",
     "fork": false,
-    "url": "https://github.com/pennbauman/test",
-    "forks_url": "https://api.github.com/repos/pennbauman/test/forks",
-    "keys_url": "https://api.github.com/repos/pennbauman/test/keys{/key_id}",
-    "collaborators_url": "https://api.github.com/repos/pennbauman/test/collaborators{/collaborator}",
-    "teams_url": "https://api.github.com/repos/pennbauman/test/teams",
-    "hooks_url": "https://api.github.com/repos/pennbauman/test/hooks",
-    "issue_events_url": "https://api.github.com/repos/pennbauman/test/issues/events{/number}",
-    "events_url": "https://api.github.com/repos/pennbauman/test/events",
-    "assignees_url": "https://api.github.com/repos/pennbauman/test/assignees{/user}",
-    "branches_url": "https://api.github.com/repos/pennbauman/test/branches{/branch}",
-    "tags_url": "https://api.github.com/repos/pennbauman/test/tags",
-    "blobs_url": "https://api.github.com/repos/pennbauman/test/git/blobs{/sha}",
-    "git_tags_url": "https://api.github.com/repos/pennbauman/test/git/tags{/sha}",
-    "git_refs_url": "https://api.github.com/repos/pennbauman/test/git/refs{/sha}",
-    "trees_url": "https://api.github.com/repos/pennbauman/test/git/trees{/sha}",
-    "statuses_url": "https://api.github.com/repos/pennbauman/test/statuses/{sha}",
-    "languages_url": "https://api.github.com/repos/pennbauman/test/languages",
-    "stargazers_url": "https://api.github.com/repos/pennbauman/test/stargazers",
-    "contributors_url": "https://api.github.com/repos/pennbauman/test/contributors",
-    "subscribers_url": "https://api.github.com/repos/pennbauman/test/subscribers",
-    "subscription_url": "https://api.github.com/repos/pennbauman/test/subscription",
-    "commits_url": "https://api.github.com/repos/pennbauman/test/commits{/sha}",
-    "git_commits_url": "https://api.github.com/repos/pennbauman/test/git/commits{/sha}",
-    "comments_url": "https://api.github.com/repos/pennbauman/test/comments{/number}",
-    "issue_comment_url": "https://api.github.com/repos/pennbauman/test/issues/comments{/number}",
-    "contents_url": "https://api.github.com/repos/pennbauman/test/contents/{+path}",
-    "compare_url": "https://api.github.com/repos/pennbauman/test/compare/{base}...{head}",
-    "merges_url": "https://api.github.com/repos/pennbauman/test/merges",
-    "archive_url": "https://api.github.com/repos/pennbauman/test/{archive_format}{/ref}",
-    "downloads_url": "https://api.github.com/repos/pennbauman/test/downloads",
-    "issues_url": "https://api.github.com/repos/pennbauman/test/issues{/number}",
-    "pulls_url": "https://api.github.com/repos/pennbauman/test/pulls{/number}",
-    "milestones_url": "https://api.github.com/repos/pennbauman/test/milestones{/number}",
-    "notifications_url": "https://api.github.com/repos/pennbauman/test/notifications{?since,all,participating}",
-    "labels_url": "https://api.github.com/repos/pennbauman/test/labels{/name}",
-    "releases_url": "https://api.github.com/repos/pennbauman/test/releases{/id}",
-    "deployments_url": "https://api.github.com/repos/pennbauman/test/deployments",
-    "created_at": 1609972725,
-    "updated_at": "2021-01-06T22:38:45Z",
-    "pushed_at": 1609972875,
-    "git_url": "git://github.com/pennbauman/test.git",
-    "ssh_url": "git@github.com:pennbauman/test.git",
-    "clone_url": "https://github.com/pennbauman/test.git",
-    "svn_url": "https://github.com/pennbauman/test",
-    "homepage": null,
-    "size": 0,
+    "url": "https://github.com/pennbauman/pennbauman.com",
+    "forks_url": "https://api.github.com/repos/pennbauman/pennbauman.com/forks",
+    "keys_url": "https://api.github.com/repos/pennbauman/pennbauman.com/keys{/key_id}",
+    "collaborators_url": "https://api.github.com/repos/pennbauman/pennbauman.com/collaborators{/collaborator}",
+    "teams_url": "https://api.github.com/repos/pennbauman/pennbauman.com/teams",
+    "hooks_url": "https://api.github.com/repos/pennbauman/pennbauman.com/hooks",
+    "issue_events_url": "https://api.github.com/repos/pennbauman/pennbauman.com/issues/events{/number}",
+    "events_url": "https://api.github.com/repos/pennbauman/pennbauman.com/events",
+    "assignees_url": "https://api.github.com/repos/pennbauman/pennbauman.com/assignees{/user}",
+    "branches_url": "https://api.github.com/repos/pennbauman/pennbauman.com/branches{/branch}",
+    "tags_url": "https://api.github.com/repos/pennbauman/pennbauman.com/tags",
+    "blobs_url": "https://api.github.com/repos/pennbauman/pennbauman.com/git/blobs{/sha}",
+    "git_tags_url": "https://api.github.com/repos/pennbauman/pennbauman.com/git/tags{/sha}",
+    "git_refs_url": "https://api.github.com/repos/pennbauman/pennbauman.com/git/refs{/sha}",
+    "trees_url": "https://api.github.com/repos/pennbauman/pennbauman.com/git/trees{/sha}",
+    "statuses_url": "https://api.github.com/repos/pennbauman/pennbauman.com/statuses/{sha}",
+    "languages_url": "https://api.github.com/repos/pennbauman/pennbauman.com/languages",
+    "stargazers_url": "https://api.github.com/repos/pennbauman/pennbauman.com/stargazers",
+    "contributors_url": "https://api.github.com/repos/pennbauman/pennbauman.com/contributors",
+    "subscribers_url": "https://api.github.com/repos/pennbauman/pennbauman.com/subscribers",
+    "subscription_url": "https://api.github.com/repos/pennbauman/pennbauman.com/subscription",
+    "commits_url": "https://api.github.com/repos/pennbauman/pennbauman.com/commits{/sha}",
+    "git_commits_url": "https://api.github.com/repos/pennbauman/pennbauman.com/git/commits{/sha}",
+    "comments_url": "https://api.github.com/repos/pennbauman/pennbauman.com/comments{/number}",
+    "issue_comment_url": "https://api.github.com/repos/pennbauman/pennbauman.com/issues/comments{/number}",
+    "contents_url": "https://api.github.com/repos/pennbauman/pennbauman.com/contents/{+path}",
+    "compare_url": "https://api.github.com/repos/pennbauman/pennbauman.com/compare/{base}...{head}",
+    "merges_url": "https://api.github.com/repos/pennbauman/pennbauman.com/merges",
+    "archive_url": "https://api.github.com/repos/pennbauman/pennbauman.com/{archive_format}{/ref}",
+    "downloads_url": "https://api.github.com/repos/pennbauman/pennbauman.com/downloads",
+    "issues_url": "https://api.github.com/repos/pennbauman/pennbauman.com/issues{/number}",
+    "pulls_url": "https://api.github.com/repos/pennbauman/pennbauman.com/pulls{/number}",
+    "milestones_url": "https://api.github.com/repos/pennbauman/pennbauman.com/milestones{/number}",
+    "notifications_url": "https://api.github.com/repos/pennbauman/pennbauman.com/notifications{?since,all,participating}",
+    "labels_url": "https://api.github.com/repos/pennbauman/pennbauman.com/labels{/name}",
+    "releases_url": "https://api.github.com/repos/pennbauman/pennbauman.com/releases{/id}",
+    "deployments_url": "https://api.github.com/repos/pennbauman/pennbauman.com/deployments",
+    "created_at": 1542783868,
+    "updated_at": "2021-01-06T00:08:31Z",
+    "pushed_at": 1610066990,
+    "git_url": "git://github.com/pennbauman/pennbauman.com.git",
+    "ssh_url": "git@github.com:pennbauman/pennbauman.com.git",
+    "clone_url": "https://github.com/pennbauman/pennbauman.com.git",
+    "svn_url": "https://github.com/pennbauman/pennbauman.com",
+    "homepage": "https://pennbauman.com",
+    "size": 12061,
     "stargazers_count": 0,
     "watchers_count": 0,
-    "language": null,
+    "language": "PHP",
     "has_issues": true,
     "has_projects": true,
     "has_downloads": true,
@@ -98,7 +99,13 @@ curl http://localhost:8070/pennbauman-com/webhook \
     "archived": false,
     "disabled": false,
     "open_issues_count": 0,
-    "license": null,
+    "license": {
+      "key": "gpl-3.0",
+      "name": "GNU General Public License v3.0",
+      "spdx_id": "GPL-3.0",
+      "url": "https://api.github.com/licenses/gpl-3.0",
+      "node_id": "MDc6TGljZW5zZTk="
+    },
     "forks": 0,
     "open_issues": 0,
     "watchers": 0,
@@ -130,19 +137,19 @@ curl http://localhost:8070/pennbauman-com/webhook \
     "type": "User",
     "site_admin": false
   },
-  "created": true,
+  "created": false,
   "deleted": false,
   "forced": false,
   "base_ref": null,
-  "compare": "https://github.com/pennbauman/test/commit/bb711f5318db",
+  "compare": "https://github.com/pennbauman/pennbauman.com/compare/7acc913918bb...99c422d64268",
   "commits": [
     {
-      "id": "bb711f5318db6593eed8ca00f0ae7d81f9d22dd7",
-      "tree_id": "6015b9f19393e2b6973ff3f0bdbbe73b1616a4b8",
+      "id": "99c422d642687a2dc2808263fba014444c3a9d7d",
+      "tree_id": "c711fbcb20fe038200fd1ce9be889332cc1684ec",
       "distinct": true,
-      "message": "1",
-      "timestamp": "2021-01-06T17:41:09-05:00",
-      "url": "https://github.com/pennbauman/test/commit/bb711f5318db6593eed8ca00f0ae7d81f9d22dd7",
+      "message": "cleaned up routes",
+      "timestamp": "2021-01-07T19:49:38-05:00",
+      "url": "https://github.com/pennbauman/pennbauman.com/commit/99c422d642687a2dc2808263fba014444c3a9d7d",
       "author": {
         "name": "Penn Bauman",
         "email": "pennbauman@protonmail.com",
@@ -154,23 +161,30 @@ curl http://localhost:8070/pennbauman-com/webhook \
         "username": "pennbauman"
       },
       "added": [
-        "README.md"
+
       ],
       "removed": [
 
       ],
       "modified": [
-
+        "Cargo.lock",
+        "Cargo.toml",
+        "scripts/build.py",
+        "src/apps/core.rs",
+        "src/apps/dnd.rs",
+        "src/apps/links.rs",
+        "src/apps/mod.rs",
+        "src/main.rs"
       ]
     }
   ],
   "head_commit": {
-    "id": "bb711f5318db6593eed8ca00f0ae7d81f9d22dd7",
-    "tree_id": "6015b9f19393e2b6973ff3f0bdbbe73b1616a4b8",
+    "id": "99c422d642687a2dc2808263fba014444c3a9d7d",
+    "tree_id": "c711fbcb20fe038200fd1ce9be889332cc1684ec",
     "distinct": true,
-    "message": "1",
-    "timestamp": "2021-01-06T17:41:09-05:00",
-    "url": "https://github.com/pennbauman/test/commit/bb711f5318db6593eed8ca00f0ae7d81f9d22dd7",
+    "message": "cleaned up routes",
+    "timestamp": "2021-01-07T19:49:38-05:00",
+    "url": "https://github.com/pennbauman/pennbauman.com/commit/99c422d642687a2dc2808263fba014444c3a9d7d",
     "author": {
       "name": "Penn Bauman",
       "email": "pennbauman@protonmail.com",
@@ -182,13 +196,21 @@ curl http://localhost:8070/pennbauman-com/webhook \
       "username": "pennbauman"
     },
     "added": [
-      "README.md"
+
     ],
     "removed": [
 
     ],
     "modified": [
-
+      "Cargo.lock",
+      "Cargo.toml",
+      "scripts/build.py",
+      "src/apps/core.rs",
+      "src/apps/dnd.rs",
+      "src/apps/links.rs",
+      "src/apps/mod.rs",
+      "src/main.rs"
     ]
   }
-}'
+}
+'
